642e9d8f6cea7bda6a88d80e5f7d42b4c33506bf
LilDev1l <lildev1l.ya@gmail.com>
2021-11-08 18:43:34 +0300

    feat: add Caesar cipher

--- task1_ciphering-cli-tool/index.js
+++ task1_ciphering-cli-tool/index.js
@@ -1,7 +1,18 @@
-import {parseInputParameters} from './parseInputParameters.js';
+import {CaesarROT1TransformStream} from './encrypt/caesar/caesarROT1TransformStream.js';
+import {pipeline} from 'stream';
 
-const inputParameters = parseInputParameters(process.argv.slice(2));
-console.log(inputParameters);
+const streamCaesarEncryption = new CaesarROT1TransformStream(true);
+const streamCaesarDecryption = new CaesarROT1TransformStream(false);
+
+pipeline(
+    process.stdin,
+    streamCaesarEncryption,
+    streamCaesarDecryption,
+    process.stdout,
+    (err => {
+        console.log(err.message);
+    })
+);
 
 
 
