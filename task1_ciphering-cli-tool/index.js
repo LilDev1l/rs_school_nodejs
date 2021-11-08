52fdf413f5a695b8a616db2235016f8845afe394
LilDev1l <lildev1l.ya@gmail.com>
2021-11-08 18:34:35 +0300

    feat: add processing input parameters

---
+++ task1_ciphering-cli-tool/index.js
@@ -0,0 +1,10 @@
+import {parseInputParameters} from './parseInputParameters.js';
+
+const inputParameters = parseInputParameters(process.argv.slice(2));
+console.log(inputParameters);
+
+
+
+
+
+
---
+++ task1_ciphering-cli-tool/parseInputParameters.js
@@ -0,0 +1,24 @@
+function parseInputParameters(args) {
+    let config, input, output;
+
+    args.forEach((parameter, index) => {
+        switch (parameter) {
+            case "-c":
+            case "--config":
+                config = args[index + 1];
+                break;
+            case "-i":
+            case "--input":
+                input = args[index + 1];
+                break;
+            case "-o":
+            case "--output":
+                output = args[index + 1];
+                break;
+        }
+    })
+
+    return {config, input, output};
+}
+
+export {parseInputParameters};
