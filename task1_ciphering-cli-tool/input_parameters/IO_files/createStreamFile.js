684488f3ce98a7f01a613075516e7ef8eeae22df
LilDev1l <lildev1l.ya@gmail.com>
2021-11-27 00:43:09 +0300

    fix: throw an error if there is no file

--- task1_ciphering-cli-tool/input_parameters/IO_files/createStreamFile.js
+++ task1_ciphering-cli-tool/input_parameters/IO_files/createStreamFile.js
@@ -1,15 +1,27 @@
-import fs from 'fs/promises';
+import fsPromises from 'fs/promises';
+import fs from 'fs';
+import {UnavailableFileError} from '../../errors/UnavailableFileError.js';
 
 async function createInputStreamFile(pathFile) {
-    const fileHandle = await fs.open(pathFile, 'r');
-    const inputStream = fileHandle.createReadStream()
+    let inputStream;
+    try {
+        await fsPromises.access(pathFile, fs.constants.R_OK);
+        inputStream = fs.createReadStream();
+    } catch (err) {
+        throw new UnavailableFileError('Could not open file for reading');
+    }
 
     return inputStream;
 }
 
 async function createOutputStreamFile(pathFile) {
-    const fileHandle = await fs.open(pathFile, 'ax');
-    const outputStream = fileHandle.createWriteStream()
+    let outputStream;
+    try {
+        await fsPromises.access(pathFile, fs.constants.W_OK);
+        outputStream = fs.createWriteStream()
+    } catch (err) {
+        throw new UnavailableFileError('Could not open file for writing');
+    }
 
     return outputStream;
 }
